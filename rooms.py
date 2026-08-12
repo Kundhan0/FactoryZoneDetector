7f3f2ae956e1685bf7b2d1430ca49ec3faf457ab
Kundhan <kundhanakkenapally@gmail.com>
2026-08-12 23:07:40 +0530

    The Prototype without errors

---
+++ rooms.py
@@ -0,0 +1,11 @@
+ROOMS = {
+    "CHEMICAL STORAGE":{"rect":(100,80,320,160),"zone":"red","camera":"camera1.mp4"},
+    "FURNACE ROOM":{"rect":(100,250,320,150),"zone":"red","camera":"camera2.mp4"},
+    "ELECTRICAL ROOM":{"rect":(420,80,300,160),"zone":"orange","camera":"camera3.mp4"},
+    "SERVER ROOM":{"rect":(740,80,300,160),"zone":"orange","camera":"camera4.mp4"},
+    "OFFICES":{"rect":(450,350,300,180),"zone":"green","camera":"office_fire.mp4"},
+    "KITCHEN":{"rect":(100,520,300,180),"zone":"yellow","camera":"camera6.mp4"},
+    "BREAK ROOM":{"rect":(420,520,320,180),"zone":"yellow","camera":"camera7.mp4"},
+    "EMERGENCY EXIT":{"rect":(920,320,500,220),"zone":"green","camera":"camera8.mp4"},
+    "OUTDOOR GARDEN":{"rect":(920,560,500,300),"zone":"green","camera":"camera9.mp4"}
+}
